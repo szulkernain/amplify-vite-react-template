3cd782a0145e9ada311e3886444b203e1df8784e
Sina Zulkernain <szulkernain@ambrygen.com>
2024-07-21 17:35:13 -0700

    new auth

--- amplify/data/resource.ts
+++ amplify/data/resource.ts
@@ -12,7 +12,7 @@ const schema = a.schema({
       content: a.string(),
     })
     .authorization(allow => [
-      allow.authenticated().to(['read']),
+      allow.guest().to(['read']),
       allow.owner(),
     ]),
 });
@@ -22,7 +22,7 @@ export type Schema = ClientSchema<typeof schema>;
 export const data = defineData({
   schema,
   authorizationModes: {
-    defaultAuthorizationMode: "apiKey",
+    defaultAuthorizationMode: "userPool",
   },
 });
 
