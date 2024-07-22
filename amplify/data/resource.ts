3180987ce6d96385d012476ffcbe6d10c00fe149
Sina Zulkernain <szulkernain@ambrygen.com>
2024-07-21 17:22:02 -0700

    trying last ditch auth

--- amplify/data/resource.ts
+++ amplify/data/resource.ts
@@ -22,7 +22,7 @@ export type Schema = ClientSchema<typeof schema>;
 export const data = defineData({
   schema,
   authorizationModes: {
-    defaultAuthorizationMode: "userPool",
+    defaultAuthorizationMode: "apiKey",
   },
 });
 
